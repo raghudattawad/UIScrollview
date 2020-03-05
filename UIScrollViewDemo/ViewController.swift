fb1e174efd07d7e438b3d39b196ae4028d4f59b1
Raghavendra <raghudattawad>
2020-03-05 18:35:19 +0530

    UIScrollviview with stackview

---
+++ UIScrollViewDemo/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  UIScrollViewDemo
+//
+//  Created by Raghavendra on 29/02/20.
+//  Copyright © 2020 To Be Success Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+   
+    
+/// UISCROLL VIEW  :
+    //  Give Equal width to UIView to scrollview
+    /// and
+        ///
+//Important:
+    //in uivew add all constraints to super view but its default added to content frame change it to Super view
+     
+    // Why Equal Width: 
+        
+        
+    }
+
+
+}
+
